5fd62b67f440dd37b711acd5e6a3c9df4ae3a663
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-03-17 17:46:00 +0100

    Add covariance wrapper

--- lab02/main.py
+++ lab02/main.py
@@ -1,5 +1,7 @@
+from typing import Literal
 from numpy.typing import NDArray
 from csv import reader as csv_reader
+from itertools import combinations
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,51 +11,78 @@ class_labels = {
     'Iris-virginica' : 2,
 }
 
+attribute_indexes = {
+    'sepal_length': 0,
+    'sepal_width': 1,
+    'petal_length': 2,
+    'petal_width': 3
+}
+
 def load(filename = 'iris.csv'): # missing type hint
     dataset = []
     labels = []
     with open(f'./assets/{filename}') as csv_file:
         for line in csv_reader(csv_file):
             sepal_length, sepal_width, petal_length, petal_width, label = line
+            sepal_length = float(sepal_length)
+            sepal_width = float(sepal_width)
+            petal_length = float(petal_length)
+            petal_width = float(petal_width)
             data_point = np.array([sepal_length, sepal_width, petal_length, petal_width])
             dataset.append(data_point.T)
             labels.append(class_labels[label])
     return (np.array(dataset).T, np.array(labels))
 
 def visualize_single_attributes(dataset, labels):
-    dataset_label0 = dataset[:, labels == 0]
-    dataset_label1 = dataset[:, labels == 1]
-    dataset_label2 = dataset[:, labels == 2]
-
-    plt.figure(label='sepal_length')
-    plt.hist(dataset_label0[0,:])
-    plt.hist(dataset_label1[0,:])
-    plt.hist(dataset_label2[0,:])
-    plt.figure(label='sepal_width')
-    plt.hist(dataset_label0[1,:])
-    plt.hist(dataset_label1[1,:])
-    plt.hist(dataset_label2[1,:])
-    plt.figure(label='petal_length')
-    plt.hist(dataset_label0[2,:])
-    plt.hist(dataset_label1[2,:])
-    plt.hist(dataset_label2[2,:])
-    plt.figure(label='petal_width')
-    plt.hist(dataset_label0[3,:])
-    plt.hist(dataset_label1[3,:])
-    plt.hist(dataset_label2[3,:])
+    for attribute, attribute_index in attribute_indexes.items():
+        plt.figure(label=attribute)
+        for label in class_labels.values():
+            plt.hist(dataset[attribute_index, labels == label])
     plt.show()
 
 def visualize_scatter(dataset, labels):
-    dataset_label0 = dataset[:, labels == 0]
-    dataset_label1 = dataset[:, labels == 1]
-    dataset_label2 = dataset[:, labels == 2]
+    # there are 4 attributes, hence there are 6 possible couples of attributes (unordered)
+    for item1, item2 in combinations(attribute_indexes.items(), 2):
+        attribute1, index1 = item1
+        attribute2, index2 = item2
+        plt.figure(label=f'{attribute1} vs {attribute2}')
+        for label in class_labels.values():
+            x, y = dataset[index1, labels == label], dataset[index2, labels == label]
+            plt.scatter(x, y)
+            plt.xlabel(attribute1)
+            plt.ylabel(attribute2)
 
+    plt.show()
 
+def empyrical_covariance(ndarray: NDArray, strategy: Literal['fast','loop','@'] = 'fast'):
+    mean = ndarray.mean(axis=1).reshape(ndarray.shape[0], 1)
+    n = ndarray.shape[1]
+    if strategy == 'fast':
+        return np.cov(ndarray)
+    elif strategy == 'loop':
+        covariance = 0
+        for sample in ndarray.T:
+            sample = sample.reshape((sample.shape[0], 1))
+            print(sample.shape, mean.shape)
+            partial = (sample - mean) * (sample - mean).T
+            covariance += partial
+        return covariance / n
+    elif strategy == '@':
+        centered_data = ndarray - mean
+        return centered_data @ centered_data.T / n
+
+def statistics(dataset: NDArray):
+    mean = dataset.mean(1).reshape(dataset.shape[0], 1)
+    centered_data = dataset - mean
+    print(dataset[:, :5])
+    print(centered_data[:, :5])
 
 def main():
+    np.set_printoptions(precision=1, sign=' ')
     dataset, labels = load()
     # visualize_single_attributes(dataset, labels)
-    visualize_scatter(dataset, labels)
+    # visualize_scatter(dataset, labels)
+    # statistics(dataset)
 
 if __name__ == '__main__':
     main()
