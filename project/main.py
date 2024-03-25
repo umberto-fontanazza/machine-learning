43fd1dc426fed06158ad640c1fc1f83c98c6cfef
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-03-25 08:43:20 +0100

    First project commit

---
+++ project/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+from csv import reader as csv_reader
+
+def load(file_path: str):
+    dataset = np.array([])
+    labels = []
+    with open(file_path) as csv_file:
+        for row in csv_reader(csv_file):
+            sample_values: list[float] = [float(value) for value in row[:-1]]
+            sample_ndarr = np.array(sample_values)
+            class_label = int(row[-1])
+            dataset = np.concatenate((dataset, sample_ndarr))
+            labels.append(class_label)
+    labels = np.array(labels)
+    dataset = dataset.reshape((len(dataset) // 6, 6)).T
+    return dataset, labels
+
+def main():
+    file_path = 'assets/trainData.csv'
+    dataset, labels = load(file_path)
+
+if __name__ == '__main__':
+    main()
