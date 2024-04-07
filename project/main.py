4f9e831bc553c7ebf4e0b160b8000621cf0b7c24
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-04-07 11:35:39 +0200

    Formatting with Ruff

--- project/main.py
+++ project/main.py
@@ -3,7 +3,8 @@ from csv import reader as csv_reader
 from matplotlib import pyplot as plt
 
 FEATURES_COUNT = 6
-IMAGE_FOLDER = '/Users/umbertofontanazza/Desktop/'
+IMAGE_FOLDER = "/Users/umbertofontanazza/Desktop/"
+
 
 def load(file_path: str):
     dataset = np.array([])
@@ -19,33 +20,45 @@ def load(file_path: str):
     dataset = dataset.reshape((len(dataset) // FEATURES_COUNT, FEATURES_COUNT)).T
     return dataset, labels
 
+
 def visualize_single_attributes(dataset, labels):
     for attribute_index in range(FEATURES_COUNT):
-        plt.figure(label = f'attribute [{attribute_index}]')
-        for label, label_name in enumerate(('counterfeit', 'legit')):
-            plt.hist(dataset[attribute_index, labels == label], alpha=.5,label=label_name, density=True)
+        plt.figure(label=f"attribute [{attribute_index}]")
+        for label, label_name in enumerate(("counterfeit", "legit")):
+            plt.hist(
+                dataset[attribute_index, labels == label],
+                alpha=0.5,
+                label=label_name,
+                density=True,
+            )
         plt.legend()
         if attribute_index in (4, 5):
-            plt.savefig(f'{IMAGE_FOLDER}attribute-[{attribute_index}]')
+            plt.savefig(f"{IMAGE_FOLDER}attribute-[{attribute_index}]")
     # plt.show()
+
 
 def save_scatter(dataset, labels):
     attributes = (4, 5)
     x_index, y_index = attributes
-    plt.figure(label = 'Scatter attributes 4 and 5')
-    for label, label_name in enumerate(('counterfeit', 'legit')):
-        plt.scatter(dataset[x_index, labels == label], dataset[y_index, labels == label], label = label_name)
-    plt.xlabel('Attribute 4')
-    plt.ylabel('Attribute 5')
+    plt.figure(label="Scatter attributes 4 and 5")
+    for label, label_name in enumerate(("counterfeit", "legit")):
+        plt.scatter(
+            dataset[x_index, labels == label],
+            dataset[y_index, labels == label],
+            label=label_name,
+        )
+    plt.xlabel("Attribute 4")
+    plt.ylabel("Attribute 5")
     plt.legend()
-    plt.savefig(f'{IMAGE_FOLDER}scatter-attr-4-5')
+    plt.savefig(f"{IMAGE_FOLDER}scatter-attr-4-5")
 
 
 def main():
-    file_path = 'assets/trainData.csv'
+    file_path = "assets/trainData.csv"
     dataset, labels = load(file_path)
     # visualize_single_attributes(dataset, labels)
     save_scatter(dataset, labels)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
