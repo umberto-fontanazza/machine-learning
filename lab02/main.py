a3348babf55b3273df56579364b6080cee29d812
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-05-13 18:35:51 +0200

    ...

--- lab02/main.py
+++ lab02/main.py
@@ -1,5 +1,16 @@
+from csv import reader
+
+
+def load():
+    with open("./iris.csv", "r") as file:
+        csv_file = reader(file)
+        for row in csv_file:
+            features, label = row[0:4], row[4]
+            print(f"{features=}\n{label=}")
+
+
 def main():
-    pass
+    load()
 
 
 if __name__ == "__main__":
