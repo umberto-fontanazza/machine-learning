3681995f2e6bb9c79e7ea37e63a6845029f08cd9
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-05-14 13:59:45 +0200

    Add load_iris()

--- lab02/main.py
+++ lab02/main.py
@@ -1,16 +1,27 @@
 from csv import reader
+from pprint import pprint
+from numpy import array, concatenate
+
+class_labels = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
 
 
-def load():
+def load_iris():
+    dataset, labels = [], []
     with open("./iris.csv", "r") as file:
         csv_file = reader(file)
         for row in csv_file:
             features, label = row[0:4], row[4]
-            print(f"{features=}\n{label=}")
+            features, label = [float(value) for value in features], class_labels.index(
+                label
+            )
+            dataset.append(array(features).reshape(4, 1))
+            labels.append(label)
+        dataset = concatenate(dataset, axis=1)
+    return dataset, array(labels)
 
 
 def main():
-    load()
+    dataset, labels = load_iris()
 
 
 if __name__ == "__main__":
