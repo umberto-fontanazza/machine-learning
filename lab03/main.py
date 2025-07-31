27cec45ccbdb1c387c9914a7f25c637ef436f04b
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-07-31 16:39:23 +0200

    Add pcs to lab03

---
+++ lab03/main.py
@@ -0,0 +1,39 @@
+from functools import cache
+from typing import cast
+
+from matplotlib.pyplot import figure, scatter, show
+from numpy import cov, ndarray
+from numpy.linalg import eigh
+from sklearn.datasets import load_iris
+
+
+@cache
+def load() -> tuple[ndarray, ndarray]:
+    load_res = cast(dict[str, ndarray], load_iris())
+    data, target = load_res["data"], load_res["target"]
+    data = data.T.copy()
+    return data, target
+
+
+def pca(data, m: int):
+    n = data.shape[0]
+    if n <= m:
+        raise ValueError(f"Dimensions cannot be reduced from {n} to {m}!")
+    empirical_cov = cov(data, bias=True)
+    _, U = eigh(empirical_cov)
+    P = U[:, ::-1][:, :m]
+    return P.T @ data  # projected_data
+
+
+def main():
+    data, target = load()
+    projected_data = pca(data, 2)
+    assert projected_data.shape[0] == 2
+    assert projected_data.shape[1] == 150
+    for cls in range(3):
+        scatter(projected_data[0, target == cls], projected_data[1, target == cls])
+    show()
+
+
+if __name__ == "__main__":
+    main()
