32a6af4fd4ed9f4d41a1f1a835c2cdc2baa3434a
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-09-01 15:16:56 +0200

    Add lab07 work

--- lab07/load.py
+++ lab07/load.py
@@ -1,10 +1,8 @@
-import sys
-
-def load_data():
+def load_data() -> tuple[list[str], list[str], list[str]]:
 
     lInf = []
 
-    f=open('data/inferno.txt', encoding="ISO-8859-1")
+    f = open("data/inferno.txt", encoding="ISO-8859-1")
 
     for line in f:
         lInf.append(line.strip())
@@ -12,7 +10,7 @@ def load_data():
 
     lPur = []
 
-    f=open('data/purgatorio.txt', encoding="ISO-8859-1")
+    f = open("data/purgatorio.txt", encoding="ISO-8859-1")
 
     for line in f:
         lPur.append(line.strip())
@@ -20,33 +18,35 @@ def load_data():
 
     lPar = []
 
-    f=open('data/paradiso.txt', encoding="ISO-8859-1")
+    f = open("data/paradiso.txt", encoding="ISO-8859-1")
 
     for line in f:
         lPar.append(line.strip())
     f.close()
-    
+
     return lInf, lPur, lPar
 
-def split_data(l, n):
+
+def split_data(lines: list[str], fraction: int = 4) -> tuple[list[str], list[str]]:
 
     lTrain, lTest = [], []
-    for i in range(len(l)):
-        if i % n == 0:
-            lTest.append(l[i])
+    for i in range(len(lines)):
+        if i % fraction == 0:
+            lTest.append(lines[i])
         else:
-            lTrain.append(l[i])
-            
+            lTrain.append(lines[i])
+
     return lTrain, lTest
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     # Load the tercets and split the lists in training and test lists
-    
+
     lInf, lPur, lPar = load_data()
 
     lInf_train, lInf_evaluation = split_data(lInf, 4)
     lPur_train, lPur_evaluation = split_data(lPur, 4)
     lPar_train, lPar_evaluation = split_data(lPar, 4)
-    
+
     print(lInf_train)
--- lab07/main.py
+++ lab07/main.py
@@ -1,5 +1,97 @@
+from itertools import product
+from pprint import pprint
+from typing import cast
+
+from load import load_data, split_data
+from numpy import array, dtype, float64, full, hstack, log, ndarray, ones, uint8, zeros
+from scipy.special import logsumexp
+from sklearn.preprocessing import normalize
+
+type F64Matrix = ndarray[tuple[int, int], dtype[float64]]
+type U8Array = ndarray[tuple[int], dtype[uint8]]
+
+
+def word_counts(train_data: list[str]) -> tuple[dict[str, int], int]:
+    count = 0
+    occurrencies: dict[str, int] = {}
+    for line in train_data:
+        for word in line.split():
+            count += 1
+            occurrencies[word] = occurrencies.get(word, 0) + 1
+    return occurrencies, count
+
+
+def train(train_data: list[list[str]], epsilon=0.001) -> list[dict[str, float64]]:
+    occurrencies: list[dict[str, int]] = []
+    counts: list[int] = []
+    all_words: set[str] = set()
+    for td in train_data:
+        occ, count = word_counts(td)
+        occurrencies.append(occ)
+        counts.append(count)
+        all_words = all_words.union(set(occ.keys()))
+    log_frequencies: list[dict[str, float64]] = []
+    for occ, count in zip(occurrencies, counts):
+        freq = {}
+
+        for word in all_words:
+            val = log((occ.get(word, 0) + epsilon), dtype=float64)
+            val -= log(count + epsilon * len(all_words))
+            freq[word] = val
+
+        log_frequencies.append(freq)
+    return log_frequencies
+
+
+def class_conditional_ll(
+    data: list[str] | str, log_frequencies: list[dict[str, float64]]
+) -> F64Matrix:
+    if isinstance(data, list):
+        return hstack([class_conditional_ll(entry, log_frequencies) for entry in data])
+    if not isinstance(data, str):
+        raise ValueError("Data must be a str!")
+    words: list[str] = data.split()
+    totals = zeros(len(log_frequencies), dtype=float64)
+    for word, (cls, log_freq) in product(words, enumerate(log_frequencies)):
+        totals[cls] += log_freq.get(word, 0)
+    return totals.reshape(-1, 1)
+
+
+def classify(
+    data: list[str],
+    log_frequencies: list[dict[str, float64]],
+    target: U8Array | None = None,
+):
+    prior = normalize(ones((len(log_frequencies), 1), dtype=float64), axis=0, norm="l1")
+    cls_ll = class_conditional_ll(data, log_frequencies)
+    joint_ll = cls_ll + prior
+    marginal_ll = cast(F64Matrix, logsumexp(joint_ll, axis=0)).reshape((1, -1))
+    cls_posterior_ll = joint_ll - marginal_ll
+    predictions = cls_posterior_ll.argmax(axis=0)
+    if target is None:
+        return predictions
+    return (predictions != target).sum() / target.size
+
+
 def main():
-    pass
+    hell, purgatory, heaven = load_data()
+    train_hell, test_hell = split_data(hell)
+    train_purgatory, test_purgatory = split_data(purgatory)
+    train_heaven, test_heaven = split_data(heaven)
+
+    binary_problems = [
+        [train_hell, test_hell, train_heaven, test_heaven],
+        [train_hell, test_hell, train_purgatory, test_purgatory],
+        [train_purgatory, test_purgatory, train_heaven, test_heaven],
+    ]
+    for train_0, test_0, train_1, test_1 in binary_problems:
+        model = train([train_0, train_1])
+        test_data = [*test_0, *test_1]
+        test_labels = array(
+            [0 if index < len(test_0) else 1 for index in range(len(test_data))]
+        )
+        accuracy = 1 - classify(test_data, model, test_labels)
+        print(f"{accuracy=}")
 
 
 if __name__ == "__main__":
