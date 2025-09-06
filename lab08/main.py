4f03ebec258fa8376a5b8c6be166a2a7336b789f
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2025-09-06 18:49:48 +0200

    Min dcf

--- lab08/main.py
+++ lab08/main.py
@@ -1,10 +1,26 @@
+from pathlib import Path
 from typing import cast
 
 from lib.mvg import Mvg
-from lib.types import F64Matrix, U8Array
-from numpy import astype, dtype, int64, ndarray, uint8
+from lib.types import F64Array, F64Matrix, U8Array
+from numpy import (
+    array,
+    astype,
+    dtype,
+    exp,
+    float64,
+    int64,
+    load,
+    log,
+    ndarray,
+    ones,
+    uint8,
+)
 from numpy.random import permutation, seed
+from scipy.special import logsumexp
 from sklearn.datasets import load_iris
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import normalize
 
 
 def load_data() -> tuple[F64Matrix, U8Array, F64Matrix, U8Array]:
@@ -36,8 +52,74 @@ def mvg_confusion():
     print(model_tied.confusion(test_data, test_target))
 
 
+def commedia_confusion():
+    cls_cond_ll = load(Path("data", "commedia_ll.npy"))
+    target = load(Path("data", "commedia_labels.npy")).astype(uint8)
+    prior = normalize(ones((3, 1), dtype=float64), norm="l1", axis=0)
+    log_prior = log(prior)
+    log_joint: F64Matrix = cls_cond_ll + log_prior
+    predicted: U8Array = log_joint.argmax(axis=0).astype(uint8)
+    print(confusion_matrix(target, predicted).T)
+
+
+def cost_aware_classify(
+    llr: F64Array, pi_true: float, cost_fn: float, cost_fp: float
+) -> U8Array:
+    log_joint = llr + log(pi_true / (1 - pi_true))
+    log_cost = log(cost_fn / cost_fp)
+    return ((log_joint + log_cost) > 0).astype(uint8)
+
+
+def binary_cost_matrix(cost_fn: float, cost_fp: float) -> F64Matrix:
+    return array([[0, cost_fn], [cost_fp, 0]])
+
+
+def bayes_risk(confusion_matrix: F64Matrix, cost: F64Matrix, prior: F64Array) -> float:
+    assert confusion_matrix.ndim == 2
+    assert cost.ndim == 2
+    assert prior.ndim == 1
+    return (
+        (cost * confusion_matrix).sum(axis=0) / confusion_matrix.sum(axis=0) * prior
+    ).sum()
+
+
+def dcf(conf_m: F64Matrix, cost: F64Matrix, prior: F64Array) -> float:
+    return bayes_risk(conf_m, cost, prior)
+
+
+def min_dcf(
+    llr: F64Array, target: U8Array, pi: float, cost_fn: float, cost_fp: float
+) -> float:
+    sorted_target = target[llr.argsort()]
+
+    predicted_labels = ones((llr.size), dtype=uint8)
+    conf_m = confusion_matrix(target, predicted_labels).T
+    cost = binary_cost_matrix(cost_fn, cost_fp)
+    prior = array([(1 - pi), pi])
+    min_dcf = dcf(conf_m, cost, prior)
+    for target in sorted_target:
+        conf_m[1, target] -= 1
+        conf_m[0, target] += 1
+        min_dcf = min(min_dcf, dcf(conf_m, cost, prior))
+    return min_dcf
+
+
 def main():
-    pass
+    cls_conditional_llr = load("data/commedia_llr_infpar.npy")
+    target = load("data/commedia_labels_infpar.npy")
+    applications = [(0.5, 1, 1), (0.8, 1, 1), (0.5, 10, 1), (0.8, 1, 10)]
+    for pi, cost_fn, cost_fp in applications:
+        predictions = cost_aware_classify(cls_conditional_llr, pi, cost_fn, cost_fp)
+        m = confusion_matrix(target, predictions).T
+        risk = bayes_risk(m, binary_cost_matrix(cost_fn, cost_fp), array([1 - pi, pi]))
+        base_risk = min(pi * cost_fn, (1 - pi) * cost_fp)
+
+        normalized_dcf = risk / base_risk
+        print(f"{normalized_dcf=}")
+
+        minimum_dcf = min_dcf(cls_conditional_llr, target, pi, cost_fn, cost_fp)
+        min_norm_dcf = minimum_dcf / base_risk
+        print(f"{min_norm_dcf=}")
 
 
 if __name__ == "__main__":
