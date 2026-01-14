228c445503d7b01df9d549d5be7ef6c240f2ebb2
Umberto Fontanazza <34311826+umberto-fontanazza@users.noreply.github.com>
2026-01-14 19:00:36 +0100

    Add DCF evaluation to SVM linear

--- lab11/lib/types.py
+++ lab11/lib/types.py
@@ -1,7 +1,8 @@
-from numpy import dtype, float64, int8, ndarray, uint8
+from numpy import dtype, float64, int8, ndarray, uint8, uint64
 
 type F64Matrix = ndarray[tuple[int, int], dtype[float64]]
 type U8Matrix = ndarray[tuple[int, int], dtype[uint8]]
 type F64Array = ndarray[tuple[int], dtype[float64]]
 type U8Array = ndarray[tuple[int], dtype[uint8]]
 type I8Array = ndarray[tuple[int], dtype[int8]]
+type U64Matrix = ndarray[tuple[int, int], dtype[uint64]]
--- lab11/main.py
+++ lab11/main.py
@@ -1,17 +1,35 @@
 from itertools import product
 
 from lib.data import load_iris
+from lib.evaluation import dcf, dcf_min_bin, uniform_cost
 from lib.svm import Svm
+from numpy import full
+from sklearn.metrics import confusion_matrix
 from solution import train_dual_SVM_linear
 
 
 def main():
     train_data, train_target, test_data, test_target = load_iris(binary=True)
 
+    n_classes = 2
+    cost = uniform_cost(n_classes)
+    prior = full((n_classes), 1 / n_classes)
+
     for K, C in product([1, 10], [0.1, 1, 10]):
         model = Svm(train_data, train_target, C, K)
         primal, dual = model.primal_loss, model.dual_loss
-        print(f"K: {K:2}\t\tC: {C:4}\t\tprimal: {primal:5.2f}\t\tdual: {dual:5.2f}")
+        scores = model.score(test_data)
+        predicted_test_target = scores > 0
+        err_rate = (predicted_test_target != test_target).sum() / test_target.size
+        err_rate_percent = 100 * err_rate
+        cm = confusion_matrix(test_target, predicted_test_target)
+
+        print(
+            f"K: {K:2}\t\tC: {C:4}\t\tprimal: {primal:5.2f}\t\tdual: {dual:5.2f}\t\t"
+            + f"err rate: {err_rate_percent:5.1f}%\t\t"
+            + f"dcf min: {dcf_min_bin(scores, test_target, cost, prior):.4f}\t\t"
+            + f"dcf: {dcf(cm, cost, prior):.4f}\t\t"
+        )
 
 
 if __name__ == "__main__":
