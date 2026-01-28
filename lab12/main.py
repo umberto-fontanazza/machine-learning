c198c36bb1e91aff4ec129667837cdb71d84e8fa
Umberto Fontanazza <34311826+umberto-fontanazza@users.noreply.github.com>
2026-01-28 13:14:48 +0100

    Verify with prof data

--- lab12/main.py
+++ lab12/main.py
@@ -1,9 +1,13 @@
+from pathlib import Path
 from typing import cast
 
+from data.GMM_load import load_gmm
 from lib.mvg import mvg_log_density
 from lib.types import F64Array, F64Matrix, F64Tensor3D
-from numpy import log, vstack
+from numpy import array, load, log, save, vstack
 from scipy.special import logsumexp
+
+DATA_PATH = Path(__file__).parent / "data"
 
 
 def logpdf_gmm(
@@ -17,7 +21,19 @@ def logpdf_gmm(
 
 
 def main():
-    pass
+    train_data_path = DATA_PATH / "GMM_data_4D.npy"
+    gmm_path = DATA_PATH / "GMM_4D_3G_init.json"
+    train_data = load(train_data_path)
+    gmm = load_gmm(gmm_path)
+    weights, means, covs = zip(*gmm)
+    weights, means, covs = (
+        array(weights),
+        array([m.ravel() for m in means]).T,
+        list(covs),
+    )
+    computed = logpdf_gmm(train_data, means, covs, weights)
+    computed = computed.reshape((1, -1))
+    save(DATA_PATH / "my_res.npy", computed)
 
 
 if __name__ == "__main__":
